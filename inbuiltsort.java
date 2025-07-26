8d19a9a8a81831d2cdf9b85375e883e29722a096
Prince Suthar <sutharprince46@gmail.com>
2025-07-26 23:07:53 +0530

    17

--- inbuiltsort.java
+++ inbuiltsort.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 public class inbuiltsort {
 
-     public static void print(Integer arr[]){
+     public static void print(int arr[]){
         for(int i = 0; i< arr.length;i++){
             System.out.print(" "+ arr[i]);
         }
@@ -12,8 +11,8 @@ public class inbuiltsort {
     }
 
     public static void main(String[] args) {
-        Integer number[]={5,2,1,3,4};
-        Arrays.sort(number,Collections.reverseOrder());
+        int number[]={5,2,1,3,4};
+        Arrays.sort(number);
         print(number);
     }
 }
