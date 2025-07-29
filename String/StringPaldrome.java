4cbbfc4a44c5bce35b6fc0824861313111635f24
Prince Suthar <sutharprince46@gmail.com>
2025-07-29 23:33:53 +0530

    Strings

---
+++ String/StringPaldrome.java
@@ -0,0 +1,21 @@
+
+
+public class StringPaldrome {
+    public static boolean palidrome(String str){
+        int n = str.length();
+      
+        for(int i =0;i<=n/2; i++){
+            if(str.charAt(i)!= str.charAt(n-i-1)){
+            return false;
+
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+    String str = "racecar";
+    System.out.println(palidrome(str));
+        
+    }
+
+}
